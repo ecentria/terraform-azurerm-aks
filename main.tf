9b5b9b200ae0d566b5b3695a3e77f5689ce96b1b
hezijie <lonegunmanb@hotmail.com>
2024-08-26 10:11:52 +0800

    use var.prefix for both dns_prefix and dns_prefix_private_cluster, based on var.private_cluster_enabled

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ resource "azurerm_kubernetes_cluster" "main" {
   automatic_channel_upgrade           = var.automatic_channel_upgrade
   azure_policy_enabled                = var.azure_policy_enabled
   disk_encryption_set_id              = var.disk_encryption_set_id
-  dns_prefix                          = var.prefix
-  dns_prefix_private_cluster          = var.dns_prefix_private_cluster
+  dns_prefix                          = var.private_cluster_enabled == true ? null : var.prefix
+  dns_prefix_private_cluster          = var.private_cluster_enabled == true ? var.prefix : false
   image_cleaner_enabled               = var.image_cleaner_enabled
   image_cleaner_interval_hours        = var.image_cleaner_interval_hours
   kubernetes_version                  = var.kubernetes_version
--- variables.tf
+++ variables.tf
@@ -454,12 +454,6 @@ variable "disk_encryption_set_id" {
   description = "(Optional) The ID of the Disk Encryption Set which should be used for the Nodes and Volumes. More information [can be found in the documentation](https://docs.microsoft.com/azure/aks/azure-disk-customer-managed-keys). Changing this forces a new resource to be created."
 }
 
-variable "dns_prefix_private_cluster" {
-  type        = string
-  default     = null
-  description = "(Optional) Specifies the DNS prefix to use with private clusters. Only one of `var.prefix,var.dns_prefix_private_cluster` can be specified. Changing this forces a new resource to be created."
-}
-
 variable "ebpf_data_plane" {
   type        = string
   default     = null
